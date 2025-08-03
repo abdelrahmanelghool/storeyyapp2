684b33e1b4cc864a37e602c6d2f2718da492e43a
abdelrahmanelghool <abdelrahman.elghool@gmail.com>
2025-08-03 22:15:56 +0300

    تعديلات الشكل

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./App.tsx",
+    "./components/**/*.{ts,tsx}",
+    "./supabase/functions/server/**/*.{ts,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
